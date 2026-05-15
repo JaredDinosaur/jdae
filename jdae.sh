434d2acb9f420ff76ca1f956335be488797a63b5
Jared Dinosaur <61011316+JaredDinosaur@users.noreply.github.com>
2026-05-16 00:15:20 +0100

    Add files via upload

---
+++ jdae.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+yay -S --needed archiso squashfs-tools xorriso
+mkdir -p archiso
+cd archiso
+cp -r /usr/share/archiso/configs/releng jdae
+cd jdae
+echo "networkmanager" >> packages.x86_64
+echo "network-manager-applet" >> packages.x86_64
+echo "wireless_tools" >> packages.x86_64
+echo "wpa_supplicant" >> packages.x86_64
+echo "iw" >> packages.x86_64
+echo "dialog" >> packages.x86_64
+echo "usbutils" >> packages.x86_64
+echo "pciutils" >> packages.x86_64
+echo "hwinfo" >> packages.x86_64
+mkdir -p airootfs/usr/local/bin
+cp jdae.sh airootfs/usr/local/bin
+chmod +x airootfs/usr/local/bin
+mkdir -p airootfs/etc/systemd/system
+echo "[Unit]" >> airootfs/etc/systemd/system/jdae.service
+echo "Description=JDAE startup script" >> airootfs/etc/systemd/system/jdae.service
+echo "After=multi-user.target" >> airootfs/etc/systemd/system/jdae.service
+echo "" >> airootfs/etc/systemd/system/jdae.service
+echo "[Service]" >> airootfs/etc/systemd/system/jdae.service
+echo "Type=simple" >> airootfs/etc/systemd/system/jdae.service
+echo "ExecStart=/usr/local/bin/jdae.sh" >> airootfs/etc/systemd/system/jdae.service
+echo "" >> airootfs/etc/systemd/system/jdae.service
+echo "[Install]" >> airootfs/etc/systemd/system/jdae.service
+echo "WantedBy=multi-user.target" >> airootfs/etc/systemd/system/jdae.service
+mkdir -p airootfs/etc/systemd/system/multi-user.target.wants
+ln -s /etc/systemd/system/jdae.service airootfs/etc/systemd/system/multi-user.target.wants/myscript.service
+ln -s /usr/lib/systemd/system/NetworkManager.service airootfs/etc/systemd/system/multi-user.target.wants/NetworkManager.service
+cd ..
+sudo rm -rf work out
+sudo mkarchiso -v jdae
